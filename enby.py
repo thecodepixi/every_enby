220f7e1c276e63c93dbb99136fbec754e81b68dc
Emily Harber <16492325+thecodepixi@users.noreply.github.com>
2021-07-16 00:32:35 -0400

    remove dotenv

--- enby.py
+++ enby.py
@@ -5,9 +5,6 @@ import string
 import random
 import time 
 
-from dotenv import load_dotenv
-load_dotenv()
-
 def twitter_api():
   twitter_api_key = os.environ["twitter_api_key"]
   twitter_api_secret = os.environ["twitter_api_secret"]
