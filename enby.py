57112e27cf0c8d4005c4d72cc8494008dc8e5f4f
Emily Harber <16492325+thecodepixi@users.noreply.github.com>
2021-07-15 23:37:16 -0400

    Words API integration working!

--- enby.py
+++ enby.py
@@ -1,5 +1,8 @@
 import tweepy
 import os
+import requests
+import string
+import random
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,5 +14,38 @@ def twitter_api():
 
   auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
   auth.set_access_token(twitter_access_token, twitter_access_secret)
-  
+
   return tweepy.API(auth)
+
+def get_random_word():
+  url = "https://wordsapiv1.p.rapidapi.com/words/"
+  parts_of_speech = ["noun","verb","adjective"]
+  random_pos = random.choice(parts_of_speech)
+  letters = list(string.ascii_lowercase)
+  random_letter = random.choice(letters)
+  letter_pattern = f'^{random_letter}' + '+\\w{3,10}'
+
+  querystring = {"limit":"100","partofspeech":random_pos,"letterPattern":letter_pattern,"frequencymin":"7.5"}
+  print(querystring)
+
+  headers = {
+      'x-rapidapi-key': os.environ["dictionary_api_key"],
+      'x-rapidapi-host': os.environ["dictionary_api_host"]
+      }
+
+  response = requests.request("GET", url, headers=headers, params=querystring)
+
+  return {
+    "pos": random_pos,
+    "word": random.choice(response.json()['results']['data'])
+  }
+
+def make_sentence():
+  enby = ["enby", "non-binary"]
+  word = get_random_word()
+  if word["pos"] == "adjective":
+    return word["word"] + " " + enby[0]
+  elif word["pos"] == "noun":
+    return enby[1] + " " + word["word"]
+  else:
+    return random.choice(enby) + " "  + word["word"]
