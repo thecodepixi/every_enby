013001caefe35e3f78b6cc6d3e4c8f4e4b44884c
Emily Harber <16492325+thecodepixi@users.noreply.github.com>
2022-04-17 12:46:52 -0400

    Update enby.py to use word lists

--- enby.py
+++ enby.py
@@ -1,12 +1,7 @@
 import tweepy
 import os
-import requests
-import string
 import random
 import time 
-# used for development
-# from dotenv import load_dotenv
-# load_dotenv()
 
 def twitter_api():
   twitter_api_key = os.environ["twitter_api_key"]
@@ -20,24 +15,14 @@ def twitter_api():
   return tweepy.API(auth)
 
 def get_random_word():
-  url = "http://api.wordnik.com/v4/words.json/randomWord"
-  api_key = os.environ["wordnik_api_key"]
   parts_of_speech = ["noun","verb","adjective","adverb"]
   random_pos = random.choice(parts_of_speech)
 
-  # minCorpusCount determins corpus frequency of the word
-  querystring = {
-    "hasDictionaryDef": True,
-    "includePartOfSpeech": random_pos,
-    "excludePartOfSpeech": "family-name,given-name",
-    "minCorpusCount": "100",
-    "minLength": 3,
-    "api_key": api_key
-  }
+  word_file = open(random_pos + ".txt", "r")
+  word_list = word_file.read().split(",")
+  word = random.choice(word_list)
 
-  response = requests.get(url, params=querystring)
-
-  return [random_pos, response.json()['word'].lower()]
+  return [random_pos, word]
 
 def make_sentence():
   enby = ["enby", "non-binary"]
